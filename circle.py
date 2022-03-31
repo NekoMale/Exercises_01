a02bdc1ed1dbacbced33f1ffc924d7ec5e6533b8
ilMale <danieleluciani92@gmail.com>
2022-03-31 11:59:10 +0200

    Circle extended
    - Added properties
    - Added __str__ method
    - Added test for scale, rescale, transtlate and translate_to

--- circle.py
+++ circle.py
@@ -4,11 +4,15 @@ class Circle:
     def __init__(self, x_center, y_center, radius):
         self.x_center = x_center
         self.y_center = y_center
-        self.radius = radius
+        self.__radius = radius
 
     @property
     def center(self):
         return self.x_center, self.y_center
+
+    @property
+    def radius(self):
+        return self.__radius
 
     def translate(self, delta_x, delta_y):
         self.x_center += delta_x
@@ -19,16 +23,16 @@ class Circle:
         self.y_center = new_y
 
     def scale(self, delta_radius):
-        self.radius += delta_radius
+        self.__radius += delta_radius
 
     def rescale(self, new_radius):
-        self.radius = new_radius
+        self.__radius = new_radius
 
     def get_circumference(self):
         return 2 * self.radius * math.pi
 
     def get_area(self):
-        return self.radius * self.radius * math.pi
+        return self.radius ** 2 * math.pi
 
     def __add__(self, other):
         return Circle(self.x_center + other.x_center, self.y_center + other.y_center, self.radius + other.radius)
@@ -47,3 +51,14 @@ class Circle:
 
     def __ge__(self, other):
         return self.get_area() >= other.get_area()
+
+    def __str__(self):
+        text_iter = iter('')
+        for i in range(-self.radius, self.radius + 1):
+            num = math.ceil(math.sqrt(self.radius ** 2 - i ** 2))
+            if i == 0:
+                txt = "".join("_" for _ in range(self.radius))
+                txt = txt[0:-int(len(str(self.center))*0.5)] + str(self.center) + txt[0:-int(len(str(self.center))*0.5)] + f' Radius: {self.radius}'
+                print(txt)
+            else:
+                print("{:^{}}".format("".join("_" for _ in range(2 * num)), 2 * self.radius))
--- test_circle.py
+++ test_circle.py
@@ -10,6 +10,18 @@ def test_translate(circle):
     circle.translate(2, 2)
     assert circle.center == (4, 4)
 
+def test_translate_to(circle):
+    circle.translate_to(5, 5)
+    assert circle.center == (5, 5)
+
+def test_scale(circle):
+    circle.scale(2)
+    assert circle.radius == 6
+
+def test_rescale(circle):
+    circle.rescale(2)
+    assert circle.radius == 2
+
 def test_get_circumference(circle):
     assert circle.get_circumference() == 2 * 4 * math.pi
 
